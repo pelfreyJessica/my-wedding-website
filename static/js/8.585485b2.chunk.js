a2f6241967670ea6e3e0cc372ac6e9ceebcbb837
pelfreyJessica <pelfreyj1021@gmail.com>
2021-09-16 20:17:16 -0400

    Updates

---
+++ static/js/8.585485b2.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpmy-wedding-website-2.1"]=this["webpackJsonpmy-wedding-website-2.1"]||[]).push([[8],{102:function(e,s,r){"use strict";r.r(s);r(1);var t=r(9),n=(r(96),r(0));s.default=function(e){return Object(n.jsxs)("div",{className:"error-404",children:[Object(n.jsx)("h1",{className:"error-message",children:"Oops, Something went wrong"}),Object(n.jsx)(t.b,{to:"/",className:"error-redirect-link",children:"Go Back to the Home Page"})]})}},96:function(e,s,r){}}]);
+//# sourceMappingURL=8.585485b2.chunk.js.map
